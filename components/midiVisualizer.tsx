828a5c057410bc03aeb1c9654888efec3421c732
Ytomi4 <kotirashuhei@gmail.com>
2023-12-14 23:24:23 +0900

    Implemented dynamic rectangle transformations in response to piano MIDI inputs

--- components/midiVisualizer.tsx
+++ components/midiVisualizer.tsx
@@ -1,35 +1,59 @@
 'use client'
 
-import { Suspense, useEffect, useState } from 'react';
+import { Suspense, useEffect, useState, useRef } from 'react';
 import { MidiMessage, parseMIDIMessageData } from '../utils/manageMidiInput';
 import { Canvas } from '@react-three/fiber';
 import Rectangles from './rectangles';
 
 export default function MidiVisualizer() {
     const [midiInputMessages, setMidiInputMessages] = useState<MidiMessage[]>([]);
+    const [noteIntensities, setNoteIntensities] = useState<number[]>(new Array(88).fill(0));
+    const decayRate = 0.1;
+    const lastUpdateTime = useRef(Date.now());
 
     useEffect(() => {
         console.log('messages', midiInputMessages, 'length', midiInputMessages.length);
     }, [midiInputMessages]);
 
     useEffect(() => {
-      const midiAccess = navigator.requestMIDIAccess();
-      midiAccess.then(access => {
-        const inputs = Array.from(access.inputs.values());
-        inputs.forEach(input => {
-          input.onmidimessage = (message) => {
-            const data: number[] = Array.from(message.data);
-            setMidiInputMessages([parseMIDIMessageData(data)]);
-            //setMidiInputMessages(prevMessages => [...prevMessages, parseMIDIMessageData(data)]);
-          };
+        const now = Date.now();
+        const elapsed = (now - lastUpdateTime.current) / 1000; // convert ms to s
+        lastUpdateTime.current = now;
+
+        setNoteIntensities(prevIntensities =>
+            prevIntensities.map(intensity => Math.max(0, intensity - decayRate * elapsed))
+        );
+    }, []);
+
+    useEffect(() => {
+        const midiAccess = navigator.requestMIDIAccess();
+        midiAccess.then(access => {
+            const inputs = Array.from(access.inputs.values());
+            inputs.forEach(input => {
+                input.onmidimessage = (message) => {
+                    const data: number[] = Array.from(message.data);
+                    const midiMessage = parseMIDIMessageData(data);
+
+                    setMidiInputMessages(prevMessages => [...prevMessages, midiMessage]);
+
+                    setNoteIntensities(prevIntensities => {
+                        const newIntensities = [...prevIntensities];
+                        if (midiMessage.noteOn) {
+                            newIntensities[midiMessage.note - 21] = midiMessage.velocity;
+                        } else {
+                            newIntensities[midiMessage.note - 21] = 0;
+                        }
+                        return newIntensities;
+                    });
+                };
+            });
+
+            return () => {
+                inputs.forEach(input => {
+                    input.onmidimessage = () => {};
+                });
+            };
         });
-  
-        return () => {
-          inputs.forEach(input => {
-            input.onmidimessage = () => {};
-          });
-        };
-      });
     }, []);
 
     return (
@@ -37,7 +61,7 @@ export default function MidiVisualizer() {
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Suspense fallback={<div>Loading...</div>}>
-            <Rectangles space={0.02} />
+            <Rectangles space={0.02} intensities={noteIntensities} />
         </Suspense>
         <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.025, 0]}>
             <planeGeometry />
--- components/rectangles.tsx
+++ components/rectangles.tsx
@@ -2,7 +2,7 @@ import { useFrame } from '@react-three/fiber'
 import { useRef } from 'react'
 import * as THREE from 'three';
 
-export default function Rectangles({space = 0.02, width = 0.01, height = 0.05, ...props}){
+export default function Rectangles({space = 0.02, width = 0.01, height = 0.05, intensities = new Array(88).fill(0), ...props}){
     const meshRef = useRef<THREE.InstancedMesh | null>(null);
     const geometry = new THREE.BufferGeometry();
     const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
@@ -11,8 +11,7 @@ export default function Rectangles({space = 0.02, width = 0.01, height = 0.05, .
 
     useFrame(() => {
         for(let i = 0; i < 88; i++) {
-            const time = performance.now() * 0.001;
-            const deltaY = Math.sin(i + time) * 0.2 + 1;
+            const deltaY = intensities[i] * 0.1 + 1;
 
             obj.position.x = (i - 44) * space;
             obj.position.y = (deltaY / 2) * height;
